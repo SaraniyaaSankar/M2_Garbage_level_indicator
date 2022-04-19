ec26f415f4bb52f08edaccf5087d4dfbe0e07e0f
SaraniyaaSankar <101622270+SaraniyaaSankar@users.noreply.github.com>
2022-04-19 14:11:43 +0530

    Update garbage.h

--- 3_Implementation/inc/garbage.h
+++ 3_Implementation/inc/garbage.h
@@ -1,1 +1,12 @@
-
+#ifndef __GARBAGE_H__
+#define __GARBAGE_H__
+#define trigpin 6
+#define echopin 5
+int led1 = A0;
+int led2 = A1;
+int led3 = A2;
+int led4 = A3;
+int led5 = A4;
+void setup();
+void loop(int duration, int distance);
+#endif /** __GARBAGE_H_ */
